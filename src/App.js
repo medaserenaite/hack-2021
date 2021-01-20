db1e824301ecdabb103b3470b0ae72502844266c
meda.serenaite <meda.serenaite@designory.com>
2021-01-20 09:51:40 -0600

    adds some styles, uses homepage for component creation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.scss';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import EmployeeScreen from './pages/Home'
+import Task from './components/Task'
 
 
 function App() {
   return (
     <div className="App">
       <div className="contentContainer">
-            <Router>
+            {/* <Router>
               <Route path="/home" component={EmployeeScreen} />
-              <Link to="/">Admin</Link><br/>
-              <Link to="/home">Employee</Link>
-            </Router>
-            
+            </Router> */}
+            <Task />
+            <Task />
+            <Task />
       </div>
     </div>
   );
---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Task() {
+    return (
+        <div className="Task">
+            <h2>Lorem Ipsum</h2>
+        </div>
+    )
+}
