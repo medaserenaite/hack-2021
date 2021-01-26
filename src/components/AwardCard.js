a27d3880b95962687ccabe22956620a788d319df
meda.serenaite <meda.serenaite@designory.com>
2021-01-25 19:59:51 -0600

    adds award card component and styles file, imports

---
+++ src/components/AwardCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import trees from '../trees.svg'
+import './styles/AwardCard.scss'
+
+export default function AwardCard() {
+    return (
+        <div className="Task">
+            
+            <div className="Task__container">
+            {/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+            <img src={trees} className="Task__icon"/>
+                <h2>Lorem Ipsum</h2>
+                <p>Ragdoll sphynx. Himalayan bengal yet balinese</p>
+            </div>
+            <button className="Task__button">Ipsum Lorem</button>
+        </div>
+    )
+}
--- src/pages/Awards.js
+++ src/pages/Awards.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import awards from '../model/awards.json'
+import AwardCard from '../components/AwardCard'
 
 export default function Awards() {
     return (
         <div>
-            
+            {/* {awards
+            .sort(() => Math.random() - 0.5).map((element) => {
+                return (
+                    <div
+                        className="image image-blank"
+                        name={element.name}
+                    />
+                    <Task />
+                );
+            })} */}
+            <AwardCard />
+            <AwardCard />
+            <AwardCard />
         </div>
     )
 }
