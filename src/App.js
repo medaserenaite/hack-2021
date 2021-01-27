ab6639adb809987f8816c6fce07ad2416fc29177
meda.serenaite <meda.serenaite@designory.com>
2021-01-26 20:46:27 -0600

    housekeeping

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Nav from './components/Nav'
-
 import Awards from './pages/Awards'
-
 import GlobalContext from "./context/GlobalContext"
 
 function InnerApp(){
@@ -13,7 +11,6 @@ function InnerApp(){
     <div className="App">
       <Nav />
       <div className="contentContainer">   
-        {/* Home screen (contains Tasks) */}
         <Route path="/home" component={Home} />
         <Route path="/awards" component={Awards} />
       </div>
@@ -23,7 +20,6 @@ function InnerApp(){
 
 
 export default function App() {
-
   return (
     <Router>
       <GlobalContext.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
-// Page a user sees when logged in
 import React from 'react'
 import Task from '../components/Task'
 
 export default function Home() {
     return (
         <div>
-            {/* conditional depending on the context  */}
             <h1>Home Screen</h1>
             <div className="Tasks-container">
               <Task />
